f68c6fe49b15754514ac4a344c3a4855de4c72bc
Hariom Trivedi <hariom.trivedi@iflair.com>
2024-09-06 12:52:18 +0530

    estrik in host site

--- companyapi/settings.py
+++ companyapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&fu82p!xci_*uqoegrmcu+f5h%q6tqyja=(w$t5-22uyy^x5po
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
